afd097b43c3ee5ec5ece88f0eecd913845c242ac
Utkarsh Ramachandra <utkarshr.dce@gmail.com>
2021-10-01 20:02:22 +0530

    making some changes

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe('pk_test_51JaYvfSG8sDkhepb10OjziWzigEhrZYy3j0WQL5cAWLB7hskG3ZYEYzJSPoAd8AVzTm3H0ni6xKzQ0wvGK3FKsTo00GSMIINB5');
 
 export const bookTour = async tourId => {
   try {
